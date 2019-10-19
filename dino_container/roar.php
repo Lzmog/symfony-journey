1cf91966c4a8868bfa4fc0a5db9539253aa36ab3
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-10-19 13:39:22 +0300

    Fifth video

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -6,37 +6,15 @@ namespace Dino\Play;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $container = new ContainerBuilder();
+$container->setParameter('root_dir', __DIR__);
 
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/config'));
 $loader->load('services.yml');
-
-//-------------------------------------------------------
-
-$handlerDefinition = new Definition('Monolog\Handler\StreamHandler');
-$handlerDefinition->setArguments(
-    [
-        __DIR__.'/din.log'
-    ]
-);
-
-$container->setDefinition('logger.stream_handler', $handlerDefinition);
-
-//-------------------------------------------------------
-
-$stdOutLoggerDefinition = new Definition('Monolog\Handler\StreamHandler');
-$stdOutLoggerDefinition->setArguments(
-    [
-        'php://stdout'
-    ]
-);
-$container->setDefinition('logger.std_out_handler', $stdOutLoggerDefinition);
 
 //-------------------------------------------------------
 
